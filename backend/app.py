d30b3b2b94e81cb2cee866fa5014aee45add7151
Gabriel Klavans <gabeklav@bu.edu>
2020-06-02 02:19:47 -0400

    Better boilerplate

--- backend/app.py
+++ backend/app.py
@@ -2,25 +2,22 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+@app.route('/echo/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    msg = request.args.get("mag", None)
 
     # For debugging
-    print(f"got name {name}")
+    print(f"got mag {msg}")
 
     response = {}
 
     # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
+    if not msg:
+        response["ERROR"] = "no message to echo"
     # Now the user entered a valid name
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        response["MESSAGE"] = f"{msg}"
 
     # Return the response in json format
     return jsonify(response)
