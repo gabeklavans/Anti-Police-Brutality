db43b147eb01d96cfe79d807ad7cb5b22daedc8e
kauii8school <nihar.gupte@ufl.edu>
2020-06-02 16:18:48 -0400

    adding tox and setup file bc for some reason I was in the wrong folder XD

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apb-kauii8school", 
+    version="0.0.1",
+    author="The APB project",
+    author_email="APB.Program11@gmail.com",
+    description="A program to help users report acts of police bruality to local authorities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kauii8school/Anti-Police-Brutality",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[]
+)
